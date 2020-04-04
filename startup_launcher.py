d0380c78539164441732f32a2823701c27fa3c00
mppatterson84 <mppatterson84@gmail.com>
2020-04-04 11:47:03 -0500

    Update startup_launcher.py

--- startup_launcher.py
+++ startup_launcher.py
@@ -1,3 +1,4 @@
 import subprocess
 
-subprocess.call(["lxterminal", "--command=python3 gamepad_car.py"])
+subprocess.call(
+    ["lxterminal", "--command=python3 gamepad_controlled_car/gamepad_car.py"])
